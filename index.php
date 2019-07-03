d7fa5e4a2da03853a9be018d8f17c3b59a94218b
Deray <33969267+LOoLzeC@users.noreply.github.com>
2019-07-03 16:29:23 +0700

    Update and rename index.html to index.php

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+    echo '<html>';
+    echo '<head>':
+    echo '    <title>ranch_19</title>';
+    echo '<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">';
+    echo '<meta name="viewport" content="device=device-width,initial-scale=1">';
+    echo '</head>';
+    echo '<body class="bg-dark">';
+    echo '    <h3 class="text-sucess">hello</h3>';
+    echo '</body>';
+    echo '</html>';
+?>
